0630615e7364f66b860aea2e12668b7d1150eeca
Jakia jerin <98599446+Jakia-Sultana-Jerin@users.noreply.github.com>
2025-07-05 12:47:06 +0600

    Add files via upload

---
+++ models/recipes.js
@@ -0,0 +1,23 @@
+// models/recipe.js
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+  id: String,
+  user: String,
+  attachment: String,
+  description: String,
+  rating:String,
+}, );
+
+const recipeSchema = new mongoose.Schema({
+  id: String,
+  title: String,
+  description: String,
+  category: String,
+  thumbUrl: String,
+  steps: [String],
+  ingredients: mongoose.Schema.Types.Mixed,
+  reviews: [reviewSchema]
+});
+
+module.exports = mongoose.model("Recipe", recipeSchema);
